b3799732d6d41cbf287d22ddc16186a7c4f51b79
Anna Belan <annabelan85@gmail.com>
2017-04-24 23:33:24 +0800

    Добавила валидацию формы

--- js/map.js
+++ js/map.js
@@ -55,7 +55,6 @@ while(offerTitles.length < 8) {
 for (var i = 0; i < 8; i++) {
 	ad.push(generateObject(avatarNumbers[i], offerTitles[i]))
 }
-console.log(ad);
 
 function getTemplate(templateId) { 
   return document.getElementById(templateId).innerHTML 
@@ -88,7 +87,6 @@ for (var i = 0; i < 8; i++) {
 	var div = document.createElement('div');
 	div.innerHTML = markerPin;
 	var elements = div.childNodes;
-	console.log(elements[1]);
 	fragment.appendChild(elements[1]);
 }
 
@@ -180,3 +178,246 @@ dialogClose.addEventListener('keydown', function(evt) {
   			closeDialog();
   		}
 });
+
+
+
+// #12 валидация
+
+function getValue(domElement){
+	if (domElement.tagName === 'INPUT'){
+		if(domElement.type !== 'radio') 
+			return domElement.value;
+	} else if(domElement.tagName === 'SELECT') {
+		return domElement.options[domElement.selectedIndex].value;
+	}
+	return null;
+}
+
+
+class FormValidator {
+	constructor(formID) {
+		this.form = document.getElementById(formID);
+		this.validators = [];
+		this.defaultValues = this.rememberDefaultValues(this.form);
+	}
+	rememberDefaultValues(form){
+		var inputs = form.getElementsByTagName('input');
+		var values = {};
+		for(var i = 0; i < inputs.length; i++){
+			values[inputs[i].id] = getValue(inputs[i]);
+		}
+		var selects = form.getElementsByTagName('select');
+		for(var i = 0; i < selects.length; i++){
+			values[selects[i].id] = getValue(selects[i]);
+		}
+		return values
+	}
+	restoreDefaultValues(form, values){
+		var keys = Object.keys(values);
+		for(let controlId of keys){
+			var control = document.getElementById(controlId);
+			if(control) this.setValue(control, values[controlId]);
+		}
+	}
+	setValue(control, value){
+		if(control.tagName == 'INPUT'){
+			control.value = value;
+		} else if(control.tagName == 'SELECT') {
+			for(var i = 0; i < control.options.length; i++){
+				if(control.options[i].value == value){
+					control.selectedIndex = i;
+					break;
+				}
+			}
+		}
+	}
+	ready(){
+		var that = this;
+		this.form.addEventListener('submit', function(event) {
+			var isValid = true;
+			for(var i = 0; i < that.validators.length; i++){
+				if(that.validators[i].isValid() === false){
+					isValid = false;
+					that.mark(that.validators[i].getValidatedControl(), false);
+				} else {
+					that.mark(that.validators[i].getValidatedControl(), true);
+				}
+			}
+			if(isValid == false) {
+				event.preventDefault();
+			} else {
+				console.log('форма валидна, отправляем...');
+				that.restoreDefaultValues(that.form, that.defaultValues);
+				event.preventDefault();
+			}
+		})
+		return this;
+	}
+	withValidators() {
+		var that = this;
+		for(var i=0; i<arguments.length; i++)  this.validators.push(arguments[i]);
+		return this;
+	}
+	mark(field, isValid) {
+		if(isValid) {
+			field.style.border = null;
+		} else {
+			field.style.border = '5px red solid';
+		}
+	}
+}
+
+class Validator {
+	constructor(elementId){
+		this.validatedContolId = elementId;
+		this.validatedContol = null;
+		this.checkers = [];
+	}
+	getValidatedControl() {
+		if (!this.validatedContol){
+			this.validatedContol = document.getElementById(this.validatedContolId);
+		}
+		return this.validatedContol;
+	}
+	setDefaultCheckers() {
+		var checkers = []
+		for(var i=0; i<arguments.length; i++) checkers.push(arguments[i]);
+		this.setCheckers(checkers);
+		return this;
+	}
+	setCheckers(checkers) {
+		this.checkers = checkers;
+	}
+	isValid(){
+		var value = getValue(this.getValidatedControl());
+		for(var i = 0; i < this.checkers.length; i++){
+			if(this.checkers[i](value) == false){
+				return false;
+			}
+		}
+		return true;
+	}
+	resetCheckersOn(elementId, getCheckersByValue){
+		var that = this;
+		var domElement = document.getElementById(elementId);
+		domElement.addEventListener('change', function(){
+			var checkers = getCheckersByValue(getValue(domElement));
+			that.setCheckers(checkers);
+		})
+	}
+}
+
+function submitGuard(formID) {
+	var formValidator = new FormValidator();
+	formValidator.submitGuard(formID);
+	return formValidator;
+}
+
+var titleChecker = function(userInput) {
+	return false;
+}
+function lengthChecker(length) {
+	return function(userInput) {
+		return true;
+	}
+}
+
+function requiredChecker(userInput) {
+	if(userInput) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function numberChecker(userInput) {
+	return !isNaN(parseFloat(userInput)) && isFinite(userInput);
+}
+
+function lengthString(min, max) {
+	return function(userInput) {
+		if(userInput.length < min) {
+			return false;
+		}
+		if(userInput.length > max) {
+			return false;
+		}
+		return true;
+	}
+}
+
+function numberRange(min, max) {
+	return function(userInput) {
+		var number = parseFloat(userInput);
+		if(number < min) {
+			return false;
+		}
+		if(number > max) {
+			return false;
+		}
+		return true;
+	} 
+}
+
+let titleValidator = new Validator('title');
+titleValidator
+	.setDefaultCheckers(requiredChecker, lengthString(30, 100));
+
+let priceValidator = new Validator('price');
+priceValidator
+	.setDefaultCheckers(requiredChecker, numberChecker, numberRange(1000, 1000000))
+	.resetCheckersOn('type', function(value){
+		if(value == 'Квартира') {
+			return [requiredChecker, numberChecker, numberRange(1000, 1000000)];
+		} else if(value == 'Лачуга') {
+			return [requiredChecker, numberChecker, numberRange(0, 1000000)];
+		} else if (value == 'Дворец') {
+			return [requiredChecker, numberChecker, numberRange(10000, 1000000)];
+		}
+	});
+
+var formValidator = new FormValidator('create_ad_form');
+formValidator.withValidators(titleValidator, priceValidator);
+formValidator.ready();
+
+// устанавливать значения при смене других значений
+
+// время заезда время выезда
+
+// 1
+var timeControl = document.getElementById('time');
+var timeoutControl = document.getElementById('timeout');
+timeControl.addEventListener('change', function(){
+	timeoutControl.selectedIndex = timeControl.selectedIndex;
+});
+
+var roomControl = document.getElementById('room_number');
+var capacityControl = document.getElementById('capacity');
+
+// 2
+var typeControl = document.getElementById('type');
+var priceControl = document.getElementById('price');
+typeControl.addEventListener('change', function(){
+	var value = typeControl.options[typeControl.selectedIndex].value;
+	var price = parseFloat(priceControl.value);
+	var minPrices = {
+		'Квартира': 1000,
+		'Лачуга': 0,
+		'Дворец': 10000
+	}
+	console.log(value, priceControl.value, minPrices[value]);
+	if(minPrices.hasOwnProperty(value)){
+		if(price < minPrices[value]){
+			priceControl.value = minPrices[value];
+		}
+	}
+})
+
+// 3
+roomControl.addEventListener('change', function(){
+	if(roomControl.options[roomControl.selectedIndex].value == '1 комната'){
+		capacityControl.selectedIndex = 1;
+	} else {
+		capacityControl.selectedIndex = 0;
+	}
+})
